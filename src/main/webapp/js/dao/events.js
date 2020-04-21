62c7e545558de6e34a79d6b4c8691575303e822a
Inés Prieto González <ipgonzalez2@esei.uvigo.es>
2020-04-21 16:03:00 +0200

    tsk4 Adds frontend lo list recent events
    Modifies index.html and adds file js/view/events.js and js/dao/events.js

---
+++ src/main/webapp/js/dao/events.js
@@ -0,0 +1,28 @@
+var EventsDAO = (function() {
+    var resourcePath = "rest/events/";
+    var requestByAjax = function(data, done, fail, always) {
+	done = typeof done !== 'undefined' ? done : function() {};
+	fail = typeof fail !== 'undefined' ? fail : function() {};
+	always = typeof always !== 'undefined' ? always : function() {};
+
+	/*let authToken = localStorage.getItem('authorization-token');
+	if (authToken !== null) {
+	    data.beforeSend = function(xhr) {
+		xhr.setRequestHeader('Authorization', 'Basic ' + authToken);
+	    };
+	}*/
+
+	$.ajax(data).done(done).fail(fail).always(always);
+    };
+
+    function EventsDAO() {
+	this.listRecentEvents = function(done, fail, always) {
+	    requestByAjax({
+		url : resourcePath + 'recent',
+		type : 'GET'
+	    }, done, fail, always);
+	};
+    }
+
+    return EventsDAO;
+})();
---
+++ src/main/webapp/js/view/events.js
@@ -0,0 +1,79 @@
+var EventsView = (function() {
+	var dao;
+	
+	// Referencia a this que permite acceder a las funciones públicas desde las funciones de jQuery.
+	var self;
+
+	var contRow = 0;
+	var rowId = 'events-row-' + contRow;
+	var rowQuery = '#' + rowId;
+	
+	function EventsView(eventsDao, listContainerId) {
+		dao = eventsDao;
+		self = this;
+		
+		this.init = function() {
+			dao.listRecentEvents(function(events) {
+				$.each(events, function(key, event) {
+					if(contRow % 3 == 0){
+						rowId = 'events-row-' + contRow;
+						rowQuery = '#' + rowId;
+						insertEventsRow($('.' + listContainerId));
+					}
+					contRow++;
+					appendToRow(event);
+				});
+			},
+			function() {
+			    	alert('No ha sido posible acceder al listado de eventos recientes.');
+			});
+			
+
+		};
+	};
+	
+	var insertEventsRow = function(parent) {
+		parent.append(
+			'<div id="' + rowId + '" class="row">\
+			</div>'
+		);
+	};
+
+	var createEventRow = function(event) {
+		return '<div class="col-lg-4">\
+					<img class="rounded-circle" src="img/internet.svg"\
+						alt="Generic placeholder image" width="140" height="140"\
+						id="event-category">\
+					<h2 id="event-title">'+ event.title + '</h2>\
+					<p id="event-description">' + event.description + '.</p>\
+					<div class="row-lg-1">\
+						<small class="text-muted" id="event-location"> ' + event.location + '</small>\
+					</div>\
+					<div class="row">\
+						<div class="col-lg-5">\
+							<small class="text-muted" id="event-hour">' + event.event_date + '</small>\
+						</div>\
+						<div class="col-lg-7 mr-0">\
+							<small class="text-muted float-right" id="event-participant">' + event.num_participants + ' \
+								people will be there</small>\
+						</div>\
+					</div>\
+					<p>\
+						<a class="btn btn-secondary mt-2" href="#" role="button">Ver\
+							detalles</a>\
+					</p>\
+		</div>';
+	};
+
+	var showErrorMessage = function(jqxhr, textStatus, error) {
+		alert(textStatus + ": " + error);
+	};
+
+	var appendToRow = function(event) {
+		$(rowQuery)
+			.append(createEventRow(event));
+	};
+	
+	return EventsView;
+	
+})();
