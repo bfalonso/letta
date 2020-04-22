7f816fd3f7ac8c7e8825f5cb9419b7a29ea2b843
Brais <bfalonso@esei.uvigo.es>
2020-04-22 18:35:27 +0200

    tsk11 Modifies my-sql-with-inserts.sql
    Adds table users with test data

--- db/mysql-with-inserts.sql
+++ db/mysql-with-inserts.sql
@@ -14,6 +14,12 @@ CREATE TABLE `letta`.`event` (
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `letta`.`users` (
+	`login` varchar(100) NOT NULL,
+	`password` varchar(64) NOT NULL,
+	PRIMARY KEY (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE USER 'letta'@'localhost' IDENTIFIED WITH mysql_native_password BY 'letta';
 GRANT ALL ON `letta`.* TO 'letta'@'localhost';
 
@@ -33,3 +39,11 @@ INSERT INTO `letta`.`event` (`id`,`title`,`description`, `category`, `location`,
 INSERT INTO `letta`.`event` (`id`,`title`,`description`, `category`, `location`, `event_date`, `creation_date`, `capacity`, `num_participants`,`duration`) VALUES (14,'Cristiano Ronaldo','Quedada de fans aférrimos del astro futbolístico luso', 'sports', 'Campo de tiro Eiróas', '2020-06-21 20:30:00', '2020-06-12 13:00:00', 13, 3, 5);
 INSERT INTO `letta`.`event` (`id`,`title`,`description`, `category`, `location`, `event_date`, `creation_date`, `capacity`, `num_participants`,`duration`) VALUES (15,'Supremas Móstoles','Reunión para recordar las joyas musicales que trajeron al mundo este trío de artistas', 'music', 'Moruno', '2020-07-31 00:00:00', '2020-07-12 13:00:00', 8, 7, 5);
 INSERT INTO `letta`.`event` (`id`,`title`,`description`, `category`, `location`, `event_date`, `creation_date`, `capacity`, `num_participants`,`duration`) VALUES (16,'The Walking Dead','Recogida de firmas para cancelar la serie', 'series', 'Facultad de Derecho', '2020-01-18 19:00:00', '2020-01-02 13:00:00', 7, 5, 5);
+
+INSERT INTO `letta`.`users` (`login`,`password`) VALUES ("ruben","b04e95d0b09d1c3846dc2c1df871b44c780a47844534e36cfee5212f181660ae");
+INSERT INTO `letta`.`users` (`login`,`password`) VALUES ("brais","d5be99ad4984766964802a1eb0211dc6cf6020f303c9bad65935f74f1b5e6930");
+INSERT INTO `letta`.`users` (`login`,`password`) VALUES ("ines","2dc899c8e59c150e3ecbb44c2b2d3550bacf41c036de624d3eb1f275120dd733");
+INSERT INTO `letta`.`users` (`login`,`password`) VALUES ("jorge","67c888af8ad80f0232832431fb0bbb478f12740ff8b451d8d4ce0238a2d8b63a");
+INSERT INTO `letta`.`users` (`login`,`password`) VALUES ("joshua","fc52fabe94c0e037d2df4498e87481a6438960c9f73d517584a7a5c564535ac4");
+INSERT INTO `letta`.`users` (`login`,`password`) VALUES ("daniel","bd3dae5fb91f88a4f0978222dfd58f59a124257cb081486387cbae9df11fb879");
+INSERT INTO `letta`.`users` (`login`,`password`) VALUES ("miguel","5ef68465886fa04d3e0bbe86b59d964dd98e5775e95717df978d8bedee6ff16c");
